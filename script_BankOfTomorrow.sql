d798b54e6511dcf6b00765515fce18ff11b94a11
mariyap <mariqpetkova98@abv.bg>
2020-02-18 14:43:31 +0200

    commit

--- script_BankOfTomorrow.sql
+++ script_BankOfTomorrow.sql
@@ -117,6 +117,150 @@ FROM Employees
 WHERE ROWNUM <= 5
 ORDER BY Salary DESC;
 
+--?	Find all employees and their addresses
+SELECT FirstName, LastName, Address
+FROM Employees;
+
+
+--?	Find all employees and their manager
+--Self Join
+SELECT A.FirstName AS EmployeeFN, A.LastName AS EmployeeLN, B.FirstName AS ManagerFN, B.LastName AS ManagerLN
+FROM Employees A, Employees B
+WHERE A.Manager_ID = B.Employee_ID
+ORDER BY A.Employee_ID;
+
+--?	Find all employees along with their manager and address
+SELECT A.FirstName AS EmployeeFN, A.LastName AS EmployeeLN, A.Address AS EmployeeAddress, B.FirstName AS ManagerFN, B.LastName AS ManagerLN, B.Address AS ManagerAddress
+FROM Employees A, Employees B
+WHERE A.Manager_ID = B.Employee_ID
+ORDER BY A.Employee_ID;
+
+--?	Find all departments and all town names as a single list
+--Full Join
+SELECT D.DepartmentName AS Department, T.TownName AS Town
+FROM Departments D
+FULL JOIN Towns T
+ON D.Town_ID = T.Town_ID
+ORDER BY D.Town_ID;
+
+--?	Find all of the employees and the manager for each of them along with the employees that do not have a manager
+--LEFT JOIN
+SELECT A.FirstName AS EmployeeFN, A.LastName AS EmployeeLN, B.FirstName AS ManagerFN, B.LastName AS ManagerLN
+FROM Employees A
+LEFT JOIN Employees B
+ON A.Manager_ID = B.Employee_ID
+ORDER BY A.Employee_ID;
+
+--?	Find all employees from “Sales” and all from “Finance” who are hired between 1995 and 2005
+SELECT E.FirstName, E.LastName, E.Hire_Date, D.DepartmentName
+FROM Employees E
+FULL JOIN Departments D
+ON E.Department_ID = D.Department_ID
+WHERE ((D.DepartmentName = 'Finance') OR (D.DepartmentName = 'Sales')) AND (EXTRACT(YEAR FROM E.Hire_Date) BETWEEN 1995 AND 2005 )
+ORDER BY E.Employee_ID;
+
+--?	Find the full name and salary of the employee that takes minimal salary in the company.
+SELECT FirstName, LastName, MIN(Salary) AS MinSalary
+FROM Employees 
+WHERE ROWNUM = 1
+GROUP BY FirstName, LastName;
+
+--?	Find the names and the salary of the employees that have a salary that is up to 10% higher than the minimum salary for the company
+SELECT FirstName, LastName, Salary
+FROM Employees
+WHERE Salary > (SELECT MIN(Salary) FROM Employees) AND  Salary <=((SELECT MIN(Salary) FROM Employees) + (SELECT MIN(Salary) FROM Employees)*0.1);
+
+--?	Find the full name salary and the department of the employees that take the minimum salary in their department.
+--UNFINISHED
+SELECT D.Department_ID, D.DepartmentName, MIN(Salary) AS MinSalary 
+FROM ( SELECT E.FirstName, E.LastName, D.Department_ID, D.DepartmentName, MIN(Salary) AS MinSalary
+       FROM Employees E
+       FULL JOIN Departments D
+       ON E.Department_ID = D.Department_ID;) AS D
+JOIN Departments D
+ON ENames.Department_ID = D.Department_ID
+GROUP BY D.Department_ID, D.DepartmentName
+ORDER BY D.Department_ID;
+
+                
+                
+--?	Find the average salary in all department list the department name and the average salary
+SELECT D.Department_ID, D.DepartmentName, AVG(Salary) AS AverageSalary 
+FROM Employees E
+FULL JOIN Departments D
+ON E.Department_ID = D.Department_ID
+GROUP BY D.Department_ID, D.DepartmentName
+ORDER BY D.Department_ID;
+
+
+--?	Find the number of employee in all the departments. List the department and the number of employees in it
+SELECT D.Department_ID, D.DepartmentName, COUNT(E. Employee_ID) AS NumberOfEmployee
+FROM Employees E
+FULL JOIN Departments D
+ON E.Department_ID = D.Department_ID
+GROUP BY D.Department_ID, D.DepartmentName
+ORDER BY D.Department_ID;
+
+
+--?	Group all employee by the manager
+--UNFINISHED
+SELECT A.FirstName AS EmployeeFN, A.LastName AS EmployeeLN, B.FirstName AS ManagerFN, B.LastName AS ManagerLN
+FROM Employees A
+JOIN (SELECT )
+ON A.Manager_ID = B.Employee_ID
+GROUP BY B.FirstName, B.LastName;
+
+
+--?	Find all employees whose names are exactly 5 characters
+SELECT FirstName, LastName
+FROM Employees
+WHERE LENGTH(FirstName) = 5
+
+--?	Create a view that shows all clients that have been in the system today
+CREATE VIEW LogToday AS
+    SELECT C.FirstName, C.LastName, C.LastLog
+    FROM Clients C
+    WHERE C.LastLog = CURRENT_DATE;
+    
+--?	Change the passwords of all clients that are absent from the system since 10.03.2010
+UPDATE Clients
+SET Client_Password = 'newpass123'
+WHERE Clients.LastLog <= TO_DATE('10-MAR-2010', 'DD-MON-YYYY')
+
+--?	Delete all clients without password
+DELETE FROM Clients
+WHERE Client_Password IS NULL
+
+--?	Display the town with max employees 
+SELECT T.TownName, COUNT(Employee_ID) AS EmployeeCount
+FROM Towns T
+FULL JOIN Departments D
+ON T.Town_ID = D.Town_ID 
+FULL JOIN Employees E
+ON E.Department_ID = D.Department_ID
+GROUP BY T.TownName
+ORDER BY EmployeeCount DESC;
+
+--Create a transaction that deletes the information from the tables, drop all the tables and reroll the transaction at the end of the process
+BEGIN
+ DELETE FROM Clients;
+ DELETE FROM Departmnets;
+ DELETE FROM Employees;
+ DELETE FROM Towns;
+ 
+ DROP TABLE Clients;
+ DROP TABLE Departments;
+ DROP TABLE Employees;
+ DROP TABLE Towns;
+ 
+ ROLLBACK;
+END
+
+
+
+
+
+
 
 
 
